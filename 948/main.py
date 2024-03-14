548a1ed964ed78ca52dac8b3d93fc746846833c4
notPlancha <andre.plancha@hotmail.com>
2024-03-14 13:32:19 +0000

    948 slow

--- 948/main.py
+++ 948/main.py
@@ -1,19 +1,22 @@
-from itertools import permutations
+from icecream import ic
+
+from more_itertools import distinct_permutations
 class Solution:
   def bagOfTokensScore(self, tokens: list[int], power: int) -> int:
     ret = 0
-    for perm in permutations(tokens):
-      try:
-        currPower = power
-        score = 0
-        for token in perm:
-          currPower, score = self.play(token, currPower, score)
-        if score > ret: ret = score
-      except StopIteration:
-        continue
+    for i in range(1, len(tokens) + 1):
+      for perm in distinct_permutations(tokens, r=i):
+        try:
+          currPower = power
+          score = 0
+          for token in perm:
+            currPower, score = self.play(token, currPower, score)
+          if score > ret: ret = score
+        except StopIteration:
+          continue
     return ret
   def play(self, token, power, score) -> tuple[int, int]:
-    if power > token:
+    if power >= token:
       power -= token
       score += 1
     elif score >= 1:
@@ -23,10 +26,10 @@ class Solution:
       raise StopIteration
     return power, score
   def test(self):
-    from icecream import ic
-    tokens = [100, 200, 300, 400]
-    power = 200
-    assert ic(self.bagOfTokensScore(tokens, power)) == 2
+    assert ic(self.bagOfTokensScore( [100, 200, 300, 400], 200)) == 2
+    assert ic(self.bagOfTokensScore( [200,100], 150)) == 1
+    assert ic(self.bagOfTokensScore( [100], 50)) == 0
+    ic(self.bagOfTokensScore( [52,65,35,88,28,1,4,68,56,95], 94))
 if __name__ == '__main__':
   s = Solution()
   s.test()
